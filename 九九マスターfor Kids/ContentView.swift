a55916aed9c78ef4caf04375593d3e63458011a5
luzhihao <luzhihao21@gmail.com>
2025-12-24 13:55:00 +0900

    Initial Commit

---
+++ 九九マスターfor Kids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  九九マスターfor Kids
+//
+//  Created by Michael on 2025/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 九九マスターfor Kids/______for_KidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ______for_KidsApp.swift
+//  九九マスターfor Kids
+//
+//  Created by Michael on 2025/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ______for_KidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
